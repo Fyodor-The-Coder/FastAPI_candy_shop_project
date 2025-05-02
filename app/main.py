111a9298b79f7c0830e2c27cc1a39b8ee082c71e
Fyodor <fyodr.konto2@gmail.com>
2025-05-02 21:54:35 +0300

    Updated main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from app.routers.user_router import router as user_router
+from app.routers.product_router import router as product_router
+from app.routers.order_router import router as order_router
 
 app = FastAPI()
+
 
 @app.get("/")
 def root():
     return {"message": "Hello, world!"}
+
+app.include_router(user_router, prefix="/auth")
+app.include_router(product_router)
+app.include_router(order_router)
