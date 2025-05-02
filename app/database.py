dc78a7e1f6bad5ee41408035719140e4536d6617
Fyodor <fyodr.konto2@gmail.com>
2025-05-02 22:00:23 +0300

    Updated database.py

--- app/database.py
+++ app/database.py
@@ -1,22 +1,16 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlmodel import create_engine, Session, SQLModel
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./candy_shop.sqlite3"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    connect_args={"check_same_thread": False},
+    echo=True
 )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
-
-# Функция для получения сессии БД
 def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+    with Session(engine) as session:
+        yield session
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
