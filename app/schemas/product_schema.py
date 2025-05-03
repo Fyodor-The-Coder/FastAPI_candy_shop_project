705750cd3589c6dddb924087975052a9c6684c4e
Fyodor <fyodr.konto2@gmail.com>
2025-05-03 10:30:23 +0300

    Updated product_schema.py

--- app/schemas/product_schema.py
+++ app/schemas/product_schema.py
@@ -1,10 +1,9 @@
-from typing import List
+from typing import List, Optional
 from pydantic import BaseModel, Field
-
 
 class ProductBase(BaseModel):
     name: str = Field(..., example="Шоколадный торт")
-    description: str | None = Field(
+    description: Optional[str] = Field(
         default=None,
         example="Нежный шоколадный торт с ягодной прослойкой"
     )
@@ -13,15 +12,27 @@ class ProductBase(BaseModel):
     ingredients: List[str] = Field(
         ...,
         example=["мука", "сахар", "какао", "яйца"],
-        min_length=3,
-        max_length=4
+        min_length=3
     )
     stock: int = Field(..., example=10, ge=0)
-
 
 class ProductCreate(ProductBase):
     pass
 
+class ProductUpdate(BaseModel):
+    name: Optional[str] = Field(None, example="Новый шоколадный торт")
+    description: Optional[str] = Field(
+        None,
+        example="Обновленный рецепт с вишней"
+    )
+    price: Optional[float] = Field(None, example=1500.0, gt=0)
+    category: Optional[str] = Field(None, example="Торты")
+    ingredients: Optional[List[str]] = Field(
+        None,
+        example=["мука", "сахар", "какао", "вишня"],
+        min_length=3
+    )
+    stock: Optional[int] = Field(None, example=15, ge=0)
 
 class ProductResponse(ProductBase):
     id: int = Field(..., example=1)
@@ -32,9 +43,19 @@ class ProductResponse(ProductBase):
             "examples": [{
                 "id": 1,
                 "name": "Шоколадный торт",
+                "description": "Нежный шоколадный торт с ягодной прослойкой",
                 "price": 1200.50,
                 "category": "Торты",
                 "ingredients": ["мука", "сахар", "какао", "яйца"],
                 "stock": 10
             }]
         }
+
+class ProductShortInfo(BaseModel):
+    id: int
+    name: str
+    price: float
+    stock: int
+
+    class Config:
+        from_attributes = True
